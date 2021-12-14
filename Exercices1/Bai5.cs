ec87479a932f1ee1b8eaa82583605b1403b4c132
NgoDinhCanh <77870743+NgoDinhCanh@users.noreply.github.com>
2021-12-14 11:18:12 +0700

    5

---
+++ Exercices1/Bai5.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercices1
+{
+    class Bai5
+    {
+        static void Main()
+        {
+            Console.WriteLine("Nhap so a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap so b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap so c");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap so d");
+            int d = Convert.ToInt32(Console.ReadLine());
+            if (a == b && b == c && c == d)
+            {
+                Console.WriteLine("Khong co gia tri phu hop ");
+
+            }
+            else
+            {
+                // Tim Min
+                int min = a;
+                if (min > b)
+                {
+                    min = b;
+                }
+                if (min > c)
+                {
+                    min = c;
+                }
+                if (min > d)
+                {
+                    min = d;
+                }
+                Console.WriteLine($"Gia tri nho nhat la {min}");
+                // Tim Max
+                int max = a;
+                if (max < b)
+                {
+                    max = b;
+                }
+                if (max < c)
+                {
+                    max = c;
+                }
+                if (max < d)
+                {
+                    max = d;
+                }
+            }
+
+        }
+    }
+}
