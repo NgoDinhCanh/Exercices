532e1cfc6c393a32866b112cfc6526f207da9d82
NgoDinhCanh <77870743+NgoDinhCanh@users.noreply.github.com>
2021-12-14 11:10:19 +0700

    Add project files.

---
+++ Exercices1/Bai2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercices1
+{
+    class Bai2
+    {
+        static void Main()
+        {
+            Console.WriteLine("Nhap hai so nguyen a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap hai so nguyen b");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int tong = a + b;
+            int hieu = a - b;
+            int tich = a * b;
+            Console.WriteLine($"Tong {a} + {b} = {tong}");
+            Console.WriteLine($"Hieu {a} - {b} = {hieu}");
+            Console.WriteLine($"Tich {a} * {b} = {tich}");
+
+            if (b != 0)
+            {
+                float thuong = (float)a / b;
+                int du = a % b;
+                Console.WriteLine($"Thuong {a} / {b} = {thuong}");
+                Console.WriteLine($"Du {a} % {b} = {du}");
+
+            } 
+            else
+            {
+                Console.WriteLine("Khong thuc hien duoc vi mau = 0 ");
+            }    
+            
+
+
+
+        }
+    }
+}
---
+++ Exercices1/Bai3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercices1
+{
+    class Bai3
+    {
+        static void Main()
+        {
+            Console.WriteLine("Nhap so a ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap so b ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if(a < b)
+            {
+                Console.WriteLine($"Ket qua la {a} < {b}");
+            }    
+            else if ( a == b)
+            {
+                Console.WriteLine($"Ket qua la {a} = {b}");
+
+            }   
+            else
+            {
+                Console.WriteLine($"Ket qua la {a} > {b}");
+            }    
+        }
+    }
+}
